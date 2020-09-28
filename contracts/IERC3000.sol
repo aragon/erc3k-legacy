6a5aa2528b3a9a746e8850db90ea9bfa2abb7350
Jorge Izquierdo <jorge@aragon.one>
2020-09-28 19:33:17 +0200

    Interface changes

--- contracts/IERC3000.sol
+++ contracts/IERC3000.sol
@@ -8,15 +8,15 @@ pragma experimental ABIEncoderV2;
 import "./ERC3000Data.sol";
 
 abstract contract IERC3000 is ERC3000Data {
-    function schedule(Action[] calldata actions, bytes calldata proof) virtual external payable returns (bytes32 actionHash);
-    event Scheduled(bytes32 indexed actionHash, address actor, Action[] actions, bytes proof, uint256 executionTime, Collateral collateral);
+    function schedule(address executor, Action[] calldata actions, uint256 nonce, bytes calldata proof) virtual external returns (bytes32 actionHash);
+    event Scheduled(bytes32 indexed actionHash, address indexed actor, address indexed executor, Action[] actions, bytes proof, uint256 executionTime, Collateral collateral);
 
-    function execute(bytes32 actionHash, Action[] calldata actions) virtual external returns (bytes[] memory execResults);
-    event Executed(bytes32 indexed actionHash, address actor, bytes execResults);
+    function execute(bytes32 actionHash, address executor, Action[] calldata actions, uint256 nonce) virtual external returns (bytes[] memory execResults);
+    event Executed(bytes32 indexed actionHash, address indexed actor, address indexed executor, bytes execResults);
 
-    function challenge(bytes32 actionHash, bytes calldata reason) virtual external payable;
-    event Challenged(bytes32 indexed actionHash, address actor, bytes reason, Collateral collateral);
+    function challenge(bytes32 actionHash, bytes calldata reason) virtual external;
+    event Challenged(bytes32 indexed actionHash, address indexed actor, bytes reason, Collateral collateral);
 
-    function veto(bytes32 actionHash, bytes calldata reason) virtual external payable;
-    event Vetoed(bytes32 indexed actionHash, address actor, bytes reason, Collateral collateral);
+    function veto(bytes32 actionHash, bytes calldata reason) virtual external;
+    event Vetoed(bytes32 indexed actionHash, address indexed actor, bytes reason, Collateral collateral);
 }
