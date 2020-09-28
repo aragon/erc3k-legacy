d866ade77b724b333adfbe7b0408abebca1e0fb4
Jorge Izquierdo <jorge@aragon.one>
2020-09-28 15:14:57 +0200

    ERC3000Data

--- contracts/IERC3000.sol
+++ contracts/IERC3000.sol
@@ -5,27 +5,18 @@
 pragma solidity ^0.6.8;
 pragma experimental ABIEncoderV2;
 
-interface IERC3000 {
-    struct Action {
-        address to;
-        uint256 value;
-        bytes data;
-    }
+import "./ERC3000Data.sol";
 
-    struct Collateral {
-        address token;
-        uint256 amount;
-    }
-
-    function schedule(Action[] calldata actions, bytes calldata proof) external payable returns (bytes32 actionHash);
+abstract contract IERC3000 is ERC3000Data {
+    function schedule(Action[] calldata actions, bytes calldata proof) virtual external payable returns (bytes32 actionHash);
     event Scheduled(bytes32 indexed actionHash, address actor, Action[] actions, bytes proof, uint256 executionTime, Collateral collateral);
 
-    function execute(bytes32 actionHash, Action[] calldata actions) external returns (bytes[] memory execResults);
+    function execute(bytes32 actionHash, Action[] calldata actions) virtual external returns (bytes[] memory execResults);
     event Executed(bytes32 indexed actionHash, address actor, bytes execResults);
 
-    function challenge(bytes32 actionHash, bytes calldata reason) external payable;
+    function challenge(bytes32 actionHash, bytes calldata reason) virtual external payable;
     event Challenged(bytes32 indexed actionHash, address actor, bytes reason, Collateral collateral);
 
-    function veto(bytes32 actionHash, bytes calldata reason) external payable;
+    function veto(bytes32 actionHash, bytes calldata reason) virtual external payable;
     event Vetoed(bytes32 indexed actionHash, address actor, bytes reason, Collateral collateral);
 }
