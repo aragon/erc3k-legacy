0ec9a6ab7a8b27b717870ed0da1d8f61a32571dc
Jorge Izquierdo <jorge@aragon.one>
2020-09-29 06:54:42 +0200

    Use ERC3kExecutor in ERC3k

--- contracts/IERC3000.sol
+++ contracts/IERC3000.sol
@@ -6,13 +6,14 @@ pragma solidity ^0.6.8;
 pragma experimental ABIEncoderV2;
 
 import "./ERC3000Data.sol";
+import "./IERC3000Executor.sol";
 
 abstract contract IERC3000 {
-    function schedule(uint256 index, address executor, ERC3000Data.Action[] memory actions, bytes memory proof) virtual public returns (bytes32 actionHash);
-    event Scheduled(bytes32 indexed actionHash, address indexed actor, address indexed executor, ERC3000Data.Action[] actions, bytes proof, uint256 index, uint256 executionTime, ERC3000Data.Collateral collateral);
+    function schedule(uint256 index, IERC3000Executor executor, ERC3000Data.Action[] memory actions, bytes memory proof) virtual public returns (bytes32 actionHash);
+    event Scheduled(bytes32 indexed actionHash, address indexed actor, IERC3000Executor indexed executor, ERC3000Data.Action[] actions, bytes proof, uint256 index, uint256 executionTime, ERC3000Data.Collateral collateral);
 
-    function execute(uint256 index, address executor, ERC3000Data.Action[] memory actions) virtual public returns (bytes[] memory execResults);
-    event Executed(bytes32 indexed actionHash, address indexed actor, address indexed executor, bytes[] execResults, uint256 index);
+    function execute(uint256 index, IERC3000Executor executor, ERC3000Data.Action[] memory actions) virtual public returns (bytes[] memory execResults);
+    event Executed(bytes32 indexed actionHash, address indexed actor, IERC3000Executor indexed executor, bytes[] execResults, uint256 index);
 
     function challenge(bytes32 actionHash, bytes memory reason) virtual public;
     event Challenged(bytes32 indexed actionHash, address indexed actor, bytes reason, ERC3000Data.Collateral collateral);
